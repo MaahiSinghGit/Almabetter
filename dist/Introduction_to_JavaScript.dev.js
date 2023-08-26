11713c8baf401817bca84d83da11e19013253dfb
MaahisinghGit <maahisingh07m@gmail.com>
2023-08-27 03:20:59 +0530

    26-08-2023

---
+++ dist/Introduction_to_JavaScript.dev.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// INTRACTION  Alert, Prompt, Confirm
+// ALERT
+var a = "Hello world ! Welcome to JavaScript World";
+alert(a); // PROMPT
+
+var name = prompt("Enter Your Name", "Manoj Singh");
+
+if (name != null) {
+  console.log("Hello " + name);
+} else {
+  console.log("You didn't enter you name, Kindly enter your name into dialog box");
+} // CONFIRM
+
+
+var OK = confirm("You have entered wrong name");
+
+if (OK) {
+  console.log("Kindly enter your name again");
+} else {
+  console.log("Thank you for your co-opratain");
+}
